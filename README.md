3fb7abbf1b63ca6cd040ced37b4bd3f89e8101a2
EverSeenTOTOTO <everseenflash@163.com>
2021-09-07 13:45:21 +0800

    feat(settings): add husky, lint-staged and commitlint

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # nodejs-ts-boilerplate
 
- A nodejs project boilerplate with typescript, rollup, jest, eslint, vscode debug support.
+ A nodejs project boilerplate with typescript, rollup, jest, eslint, husky, lint-staged, commitlint, vscode debug support.
 
 ## Install
 
 ```bash
+npm install commitizen -g
 git clone https://github.com/EverSeenTOTOTO/nodejs-ts-boilerplate.git <your project name> --depth 1
 cd <your project name>
 yarn
--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,32 @@
 import { babel } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import typescript from '@rollup/plugin-typescript';
 import alias from '@rollup/plugin-alias';
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import path from 'path';
 
+const extensions = ['.mjs', '.js', '.ts', '.json', '.node'];
 const opts = {
   plugins: [
     json(),
     alias({
       entries: [
-        { find: '@', replacement: 'src' },
-        { find: '@test', replacement: 'src/__tests__' },
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+        { find: '@test', replacement: path.resolve(__dirname, 'src/__tests__') },
       ],
     }),
     resolve({
-      extensions: ['.mjs', '.js', '.ts', '.json', '.node'],
+      extensions,
     }),
     commonjs(),
     babel({
       babelHelpers: 'runtime',
+      extensions,
     }),
     terser({
       mangle: true,
       compress: true,
-    }),
-    typescript({
-      tsconfig: 'tsconfig.rollup.json',
     }),
   ],
 };
