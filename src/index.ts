2c40f8e5907a4c399b52ba7275a009eebc655596
EverSeenTOTOTO <everseenflash@163.com>
2022-02-26 14:41:14 +0800

    feat: add jest debug

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import 'reflect-metadata'; // reflect metadata support, uninstall if you do not need
-import dotenv from 'dotenv'; // dotenv support, uninstall if you do not need
-
 import { world } from '@/aliase';
+import dotenv from 'dotenv'; // dotenv support, uninstall if you do not need
+import 'reflect-metadata'; // reflect metadata support, uninstall if you do not need
 
 dotenv.config();
 
